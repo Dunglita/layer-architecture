1c164d06a2bbf664807f6f1b8475492c39378216
Dani <garciadanos@gmail.com>
2022-01-14 09:17:45 -0300

    Created controller's index file

---
+++ Controllers/Index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  user: require("./usercontroller"),
+  todo: require("./todocontroller"),
+  todoState: require("./todostatecontroller"),
+  folder: require("./foldercontroller"),
+};
