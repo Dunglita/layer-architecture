8b1a24d5eb00539be69d81a79235afd8f24ca163
Dani <garciadanos@gmail.com>
2022-01-14 09:48:29 -0300

    Created connection to MySQL database file

---
+++ Repositories/MySQL/ConnectionDB.js
@@ -0,0 +1,18 @@
+const mysql = require(`mysql`);
+require("dotenv").config();
+const config = require("../../Config/Environments");
+//Information to connect to MySQL database
+const connection = mysql.createConnection({
+  host: config.DB.host,
+  user: config.DB.user,
+  password: config.DB.password,
+  database: config.DB.database,
+});
+//Connection to DB
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(
+    `Successfully connected to ${config.DB.database} MySQL database:`
+  );
+});
+module.exports = connection;
